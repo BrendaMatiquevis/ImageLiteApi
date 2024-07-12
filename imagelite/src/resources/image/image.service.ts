1f0a00592a1ee5352b52852dc8e54c8bed4bf55e
Brenda Matiquevis <brenda_matiquevis@hotmail.com>
2024-07-12 16:38:06 -0300

    Criando formulário

--- imagelite/src/resources/image/image.service.ts
+++ imagelite/src/resources/image/image.service.ts
@@ -4,10 +4,18 @@ class ImageService {
     baseURL: string = 'http://localhost:8080/v1/images';
 
     async buscar(query: string = "", extension: string = "") : Promise<Image[]> {
-
         const url = `${this.baseURL}?query=${query}&extension=${extension}`
         const response = await fetch(url);
         return await response.json();
+    }
+
+    async salvar(dados: FormData) : Promise<string> {
+        const response = await fetch(this.baseURL, {
+            method: 'POST',
+            body: dados
+        });
+
+        return response.headers.get('location') ?? ''
     }
 }
 
